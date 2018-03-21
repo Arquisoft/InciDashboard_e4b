c1091c70e436c1bef90c82d6f9aac2f88cf020fe
troken11 <UO251857@uniovi.es>
2018-03-21 23:53:27 +0100

    Implementación OperarioController

---
+++ src/main/java/com/uniovi/controller/OperarioController.java
@@ -0,0 +1,29 @@
+package com.uniovi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.uniovi.entities.Operario;
+import com.uniovi.services.OperarioService;
+
+@Controller
+public class OperarioController {
+	
+	@Autowired
+	OperarioService operarioService;
+	
+    @RequestMapping(value="operario/list")
+    public String getOperarioList(Model model) {
+    	UserDetails logged = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    	Operario operario = operarioService.findByDni(logged.getUsername());
+    	
+    	model.addAttribute("nombre", operario.getNombre());
+    	model.addAttribute("incidencias", operario.getIncidencias());
+    	
+		return "operario/list";
+    }
+}
