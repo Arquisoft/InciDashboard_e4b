bb0fb20ed508929b4f6dc4eae2d14d7c6a77cc59
AlegnaLav <alegnalav@gmail.com>
2018-03-21 23:10:08 +0100

    Añadido formulario login

--- src/main/java/com/uniovi/WebSecurityConfig.java
+++ src/main/java/com/uniovi/WebSecurityConfig.java
@@ -24,21 +24,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-		.csrf().disable()
-		.authorizeRequests()
-		.antMatchers("/css/**", "/img/**", "/script/**", "/", "/register", "/login/**").permitAll()
-		.anyRequest().authenticated()
-		.and()
-		.formLogin()
-		.loginPage("/login")
-		.permitAll()
-		.defaultSuccessUrl("/")
-		.and()
-		.logout()
-		.permitAll();
+		http.csrf().disable().authorizeRequests()
+				.antMatchers("/css/**", "/img/**", "/script/**", "/", "/register", "/login/**")
+					.permitAll()
+				.anyRequest()
+					.authenticated()
+				.and()
+				.formLogin()
+					.loginPage("/login")
+					.permitAll()
+					.defaultSuccessUrl("/")
+					.failureUrl("/login?error")
+				.and()
+					.logout()
+					.permitAll();
 	}
-
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
