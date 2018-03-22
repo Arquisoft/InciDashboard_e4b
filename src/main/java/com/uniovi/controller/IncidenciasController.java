ceaeda5de4fa38f00fd5c0b944a7ce75c5b37646
AlegnaLav <alegnalav@gmail.com>
2018-03-22 19:25:24 +0100

    casi acabando cambio de estado

--- src/main/java/com/uniovi/controller/IncidenciasController.java
+++ src/main/java/com/uniovi/controller/IncidenciasController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.uniovi.entities.Incidencia;
 import com.uniovi.repositories.IncidenceRepository;
@@ -25,5 +27,24 @@ public class IncidenciasController {
 			return "incidencias/map";
 		}
 		return "redirect:/";
+	}
+	
+	@RequestMapping(value = "/incidence/update", method=RequestMethod.GET)
+	public String updateStatus(@RequestParam(defaultValue = "") Long id, 
+				@RequestParam(defaultValue = "") String estado) {
+//		Incidencia incidencia = 
+//		Usuario usuario = usersService.getUserById(id);
+//		List<Publicacion> publicaciones = postService.getPostFriends(usuario);
+//		if(publicaciones == null)
+//			model.addAttribute("noAmigo", true);
+//		else {
+//			model.addAttribute("noAmigo", false);
+//		}
+//		model.addAttribute("postsList", publicaciones);
+//		model.addAttribute("activeUserPublication", false);
+//		model.addAttribute("nombreUsuario", usuario.getName());
+//		return "publications/list";
+		System.err.println(estado);
+		return null;
 	}
 }
